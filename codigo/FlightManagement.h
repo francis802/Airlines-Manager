829ef254d9a4b4e9097c93a2386d3fa2ce683c01
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2023-01-08 10:02:46 +0000

    documentação de graph e flight management

--- codigo/FlightManagement.h
+++ codigo/FlightManagement.h
@@ -10,21 +10,81 @@
 
 using namespace std;
 
+/**
+ * Classe Flight Management
+ *
+ * Classe usada para auxiliar o tratamento dos dados
+ */
 class FlightManagement {
 public:
+    /**
+     * Construtor da classe FlightManagement
+     * @param airports path do ficheiro csv com informação relativa aos aeroportos
+     * @param airlines path do ficheiro csv com informação relativa às companhias aéreas (airlines)
+     * @param flights path do ficheiro com informação relativa aos voos
+     */
     FlightManagement(string airports, string airlines, string flights);
 
+    /**
+     * Recolhe os dados dos aeroportos a partir de um ficheiro
+     * Complexidade: O(n)
+     * @param filename Ficheiro com informações dos aeroportos
+     * @return
+     */
     int getDataAirport(string filename);
+
+    /**
+     * Recolhe os dados das companhias aéreas a partir de um ficheiro
+     * Complexidade: O(n)
+     * @param filename Ficheiro com informações das companhias aéreas
+     */
     void getDataAirline(string filename);
+
+    /**
+     * Recolhe os dados dos voos a partir de um ficheiro. Responsável por adicionar edges ao grafo.
+     * Complexidade: O(n)
+     * @param filename Ficheiro com informações dos voos
+     */
     void getDataFlights(string filename);
 
+    /**
+     * Lê as preferências de companhias aéreas guardadas no ficheiro 'preferences'
+     * Complexidade: O(n)
+     * @return Preferências de companhias aéreas
+     */
     unordered_set<string> readPreferences();
+
+    /**
+     * Atualiza o ficheiro preferences com base nas preferências guardadas
+     * Complexidade: O(n)
+     * @param preferences Preferências de companhias aéreas
+     */
     void savePreferences(unordered_set<string> preferences);
 
+    /**
+     * Getter do unordered map AirportNode
+     * @return unordered map em que a chave é um aeroporto e o valor o número do node correspondente no grafo
+     */
     const unordered_map<Airport, int, AirportHash> &getAirportNode() const;
+    /**
+     * Getter do unordered map NodeAirport
+     * @return unordered map em que a chave é o número de node no grafo e o valor o aeroporto correspondente
+     */
     const unordered_map<int, const Airport *> &getNodeAirport() const;
+    /**
+     * Getter do unordered set Airlines
+     * @return unordered set contendo as companhias aéreas
+     */
     const unordered_set<Airline, AirlineHash> &getAirlines() const;
+    /**
+     * Getter do unordered map CityAirports
+     * @return unordered map em que a chave é uma cidade e o valor uma lista de apontadores de aeroportos
+     */
     const unordered_map<string, list<const Airport *>> &getCityAirports() const;
+    /**
+     * Getter do grafo
+     * @return Grafo utilizado para representar os voos
+     */
     Graph &getFlights();
 
 private:
--- codigo/Graph.h
+++ codigo/Graph.h
@@ -10,13 +10,26 @@
 #include "Airline.h"
 #include "Airport.h"
 
+/**
+ * Classe Graph
+ *
+ * Classe usada para representar os voos
+ */
 class Graph {
 private:
+    /**
+     * Struct utilizada para representar uma aresta do grafo.
+     * Uma aresta do grafo é um voo com um determinado destino e companhia aérea.
+     */
     struct Edge{
         int dest;
         const Airline* airline;
     };
 
+    /**
+     * Struct utilizado para representar um vértice do grafo.
+     * Um vértice do grafo é um aeroporto.
+     */
     struct Node{
         list<Edge> adj;
         bool visited;
@@ -35,20 +48,83 @@ private:
     vector<Node> nodes;
 
 public:
+    /**
+     * Construtor default da classe Graph
+     */
     Graph();
+    /**
+     * Construtor da classe Graph
+     * @param num Número de nodes/vértices do grafo
+     */
     Graph(int num);
 
+    /**
+     * Caso seja fornecido um voo válido, este é adicionado ao grafo
+     * @param src Aeroporto de origem
+     * @param dest Aeroporto de Destino
+     * @param airline Companhia Aérea
+     */
     void addFlight(int src, int dest, const Airline* airline);
 
+    /**
+     * Realiza uma pesquisa em largura (Breadth-first search) respeitando as preferências do utilizador.
+     * Para cada node visitado regista a distância (número de nodes percorridos até lá chegar) e o path (node(s) a partir dos quais se atingiu o node atual)
+     * Complexidade: O(|V|+|E|)
+     * @param q Nodes a partir dos quais se inicia o bfs
+     * @param preferences Preferências do utilizador relativas às Airlines
+     */
     void bfs(queue<int> q, unordered_set<string> preferences);
+
+    /**
+     * Realiza uma pesquisa em profundidade (Depth-first search) para encontrar os pontos de articulação do grafo.
+     * Complexidade: O(|V|+|E|)
+     * @param v Node a partir do qual se inicia a pesquisa
+     * @param S Stack para guardar quais os elementos que estão a ser pesquisados
+     * @param index número da visita atual
+     */
     void dfs_art(int v, stack<int> *S, int index);
+
+    /**
+     * Define os campos dos Nodes do grafo adequadamente, de modo a que seja realizada uma pesquisa por pontos de articulação em todos os Nodes.
+     * @return Vetor contendo pontos de articulação
+     */
     vector<int> getGlobalArticulationPoints();
+
+    /**
+     * Define os campos dos Nodes do grafo adequadamente, de modo a que seja realizada uma pesquisa por pontos de articulação nos Nodes de um determinado continente.
+     * @param map Map em que a chave é o número do node e o valor o aeroporto correspondente
+     * @param countries Unordered set dos países do continente
+     * @return Vetor contendo pontos de articulação no continente
+     */
     vector<int> getContinentalArticulationPoints(unordered_map<int, const Airport*> map, unordered_set<string> countries);
+
+    /**
+     * Define os campos dos Nodes do grafo adequadamente, de modo a que seja realizada uma pesquisa por pontos de articulação nos Nodes de um determinado país.
+     * @param map Map em que a chave é o número do node e o valor o aeroporto correspondente
+     * @param countries País a fazer a pesquisa
+     * @return Vetor contendo pontos de articulação no país
+     */
     vector<int> getCountryArticulationPoints(unordered_map<int, const Airport*> map, string country);
 
+    /**
+     * Determina qual a distância de um nó inicial até ao nó mais disntante de si
+     * Complexidade: O(|V|+|E|)
+     * @param u Nó inicial
+     * @return Valor da distância de u até o nó mais distante de si
+     */
     int getMaxDistance(int u);
+
+    /**
+     * Determina o diâmetro do grafo, isto é, a maior distância entre dois nós
+     * Complexidade O(|V|*(|V|+|E|))
+     * @return Diâmetro do grafo
+     */
     int getGlobalDiameter();
 
+    /**
+     * Getter dos nodes do grafo
+     * @return Nodes do grafo
+     */
     vector<Node> getNodes();
 };
 

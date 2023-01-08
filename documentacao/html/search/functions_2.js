48126eb35183515c40afbb67e8c847f615eb2934
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2023-01-08 22:36:07 +0000

    forgot to regenerate html

--- documentacao/html/search/functions_2.js
+++ documentacao/html/search/functions_2.js
@@ -1,7 +1,13 @@
 var searchData=
 [
   ['continentalarticulationpoints_0',['continentalArticulationPoints',['../class_menu_network_stats.html#a59aece51eb706eaa1a4e74435f39e5f5',1,'MenuNetworkStats']]],
-  ['continentalstatsmenu_1',['continentalStatsMenu',['../class_menu_network_stats.html#ab34ff7ba79595101c6156a18ab2e3ebf',1,'MenuNetworkStats']]],
-  ['countryarticulationpoints_2',['countryArticulationPoints',['../class_menu_network_stats.html#a972dd476099a2b249892f40e27cbb725',1,'MenuNetworkStats']]],
-  ['countrystatsmenu_3',['countryStatsMenu',['../class_menu_network_stats.html#a5148e4b6fb49dce71e66634e31c19b49',1,'MenuNetworkStats']]]
+  ['continentalconnectedcomponents_1',['continentalConnectedComponents',['../class_menu_network_stats.html#a97201926f91720378dce57d5e16ce762',1,'MenuNetworkStats']]],
+  ['continentaldiameter_2',['continentalDiameter',['../class_menu_network_stats.html#af0c0026d8d771f04076ff79fc04c6aca',1,'MenuNetworkStats']]],
+  ['continentalscc_3',['continentalSCC',['../class_menu_network_stats.html#ab86b2bfeff48d03a0bfb5f5cbc6be183',1,'MenuNetworkStats']]],
+  ['continentalstatsmenu_4',['continentalStatsMenu',['../class_menu_network_stats.html#ab34ff7ba79595101c6156a18ab2e3ebf',1,'MenuNetworkStats']]],
+  ['countryarticulationpoints_5',['countryArticulationPoints',['../class_menu_network_stats.html#a972dd476099a2b249892f40e27cbb725',1,'MenuNetworkStats']]],
+  ['countryconnectedcomponents_6',['countryConnectedComponents',['../class_menu_network_stats.html#ac8b14855e22857701a2f166f71daf831',1,'MenuNetworkStats']]],
+  ['countrydiameter_7',['countryDiameter',['../class_menu_network_stats.html#ada447f5b5c8dbc255a95cd8fe6624e2d',1,'MenuNetworkStats']]],
+  ['countryscc_8',['countrySCC',['../class_menu_network_stats.html#a6426fec69bb2870caa22f4f1ce16fd4b',1,'MenuNetworkStats']]],
+  ['countrystatsmenu_9',['countryStatsMenu',['../class_menu_network_stats.html#a5148e4b6fb49dce71e66634e31c19b49',1,'MenuNetworkStats']]]
 ];
--- documentacao/html/search/functions_3.js
+++ documentacao/html/search/functions_3.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['dfs_5fart_0',['dfs_art',['../class_graph.html#ac12252e25ec2b65c4fab47b8839621c2',1,'Graph']]],
-  ['directairportsavailable_1',['directAirportsAvailable',['../class_menu_info.html#a110cd39f70e41007265a88f9166e9340',1,'MenuInfo']]],
-  ['directcountriesavailable_2',['directCountriesAvailable',['../class_menu_info.html#afe09d5c53cc6c1ccd1851193e3b2bcd8',1,'MenuInfo']]],
-  ['directdestinationsavailable_3',['directDestinationsAvailable',['../class_menu_info.html#a22e22720881355fa98a79a4761ca11bf',1,'MenuInfo']]],
-  ['directsearch_4',['directSearch',['../class_menu_info.html#acb5170cfff99d28835b8243876832633',1,'MenuInfo']]]
+  ['dfs_5fcc_1',['dfs_cc',['../class_graph.html#a1582815eb4aa7f789977b5b626a00b94',1,'Graph']]],
+  ['dfs_5fscc_2',['dfs_scc',['../class_graph.html#a79a39f4cec1d85fe8e0030a9ad4c3ae4',1,'Graph']]],
+  ['directairportsavailable_3',['directAirportsAvailable',['../class_menu_info.html#a110cd39f70e41007265a88f9166e9340',1,'MenuInfo']]],
+  ['directcountriesavailable_4',['directCountriesAvailable',['../class_menu_info.html#afe09d5c53cc6c1ccd1851193e3b2bcd8',1,'MenuInfo']]],
+  ['directdestinationsavailable_5',['directDestinationsAvailable',['../class_menu_info.html#a22e22720881355fa98a79a4761ca11bf',1,'MenuInfo']]],
+  ['directsearch_6',['directSearch',['../class_menu_info.html#acb5170cfff99d28835b8243876832633',1,'MenuInfo']]]
 ];
